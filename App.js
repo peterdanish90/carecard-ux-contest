52c9181fb92537504d4f745a7d0104ff80ad04ef
Vinay <vinaysinghsatna01@gmail.com>
2018-08-09 01:34:31 +0530

    Add arial font

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Font } from 'expo';
 import { StyleSheet, View } from 'react-native';
 import { Header } from 'react-native-elements';
 import RootNavigator from './src/navigator';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false
+  }
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Arial': require('./src/assets/fonts/Arial.ttf')
+    });
+    this.setState({ fontLoaded: true });
+  }
   render() {
+    if (!this.state.fontLoaded) return null;
     return (
       <View style={styles.container}>
         <Header
