232e119d5b8b11667b6037e5dabfd00fd36a2ccd
Vinay <vinaysinghsatna01@gmail.com>
2018-08-09 01:32:37 +0530

    Add theme dir and colors

---
+++ src/theme/Colors.js
@@ -0,0 +1,8 @@
+/* Colors used in whole application */
+/* Start */
+export const primaryColor = '#F24E29';
+export const blackColor = 'black';
+export const whiteColor = 'white';
+export const lightgreyColor = '#717171';
+/* Colors used in whole application */
+/* Stop */
