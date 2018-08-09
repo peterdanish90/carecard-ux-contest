50a802b9be4e9b540914ba2a62ab4f47bf163a36
Vinay <vinaysinghsatna01@gmail.com>
2018-08-10 03:19:26 +0530

    Implement info and flip card action for MyMood card

--- src/cards/MyMood/index.js
+++ src/cards/MyMood/index.js
@@ -7,6 +7,8 @@ import { animojis } from '../../local';
 import Header from '../../components/Header';
 import CaptionImage from '../../components/CaptionImage';
 import { CARDS_NAME } from '../../constant';
+import BackCard from '../../components/BackCard';
+import { CARDS_INFO } from '../../constant/cards';
 
 const flip = {
   0: {
@@ -34,14 +36,14 @@ class MyMood extends Component {
     })
     setTimeout(() => {
       this.setState({isFlipping: false})
-    }, 1000)
+    }, 500)
   }
 
   render() {
     return (
       <View style={style.container}>
         <Animatable.View animation={this.state.isFlipping ? flip : ''} style={style.cardContainer} duration={1000}>
-          {!this.state.isFlipping && (
+          {this.state.isFront && (
             <CardButtons
               // flip={this.handleRotate}
               onPressTRC= {this.handleRotate} // info
@@ -49,33 +51,44 @@ class MyMood extends Component {
               onPressBLC= {() => this.props.navigation.goBack()}
             />
           )}
-          <Header
-            style={{ height: 100 }}
-            title="MY MOOD TODAY"
-          />
-          <View style={style.body}>
-          <ScrollView>
-            <View style={style.innerBody}>
-              {
-                this.state.animoji.moods.map(mood => {
-                  return(
-                    <CaptionImage
-                      key={mood.mood_acronym}
-                      animoji={mood}
-                      onPress={myMood => this.props.navigation.navigate(CARDS_NAME.active_mood, {
-                        myMood,
-                        animoji: this.state.animoji,
-                      })}
-                      containerStyle={style.iconContainer}
-                      iconStyle={style.icon}
-                      textStyle={style.iconCaption}
-                    />
-                  );
-                })
-              }
+          {
+            this.state.isFront ?
+            <Header
+              style={{ height: 60, marginTop: 40 }}
+              title={CARDS_INFO.my_mood.title}
+            /> :
+            <BackCard
+              title={CARDS_INFO.my_mood.title}
+              desc={CARDS_INFO.my_mood.desc}
+              onClose={this.handleRotate}
+            />
+          }
+          {
+            this.state.isFront &&
+            <View style={style.body}>
+              <ScrollView>
+                <View style={style.innerBody}>
+                  {
+                    this.state.animoji.moods.map(mood => {
+                      return(
+                        <CaptionImage
+                          key={mood.mood_acronym}
+                          animoji={mood}
+                          onPress={myMood => this.props.navigation.navigate(CARDS_NAME.active_mood, {
+                            myMood,
+                            animoji: this.state.animoji,
+                          })}
+                          containerStyle={style.iconContainer}
+                          iconStyle={style.icon}
+                          textStyle={style.iconCaption}
+                        />
+                      );
+                    })
+                  }
+                </View>
+              </ScrollView>
             </View>
-          </ScrollView>
-          </View>
+          }
         </Animatable.View>
       </View>
     )
