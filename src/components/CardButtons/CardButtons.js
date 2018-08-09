44b9f582599ab653ac21a6c61f057813d4396335
Vinay <vinaysinghsatna01@gmail.com>
2018-08-09 16:05:00 +0530

    Update CardButtons component

--- src/components/CardButtons/CardButtons.js
+++ src/components/CardButtons/CardButtons.js
@@ -1,46 +1,73 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View, TouchableHighlight } from 'react-native'
+import { View, TouchableOpacity } from 'react-native'
 import FIcon from 'react-native-vector-icons/Feather'
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons'
+import FAIcon from 'react-native-vector-icons/FontAwesome'
 import style from './style';
 
 const CardButtons = props => {
   return (
     <View style={style.buttonContainer}>
-      <TouchableHighlight style={[style.cardButton, style.topLeft]}>
-        <FIcon
-          // name="refresh-ccw"
-          size={18}
-          style={style.buttonIconTopleft}
-          color={props.buttons.topLeft.disabled ? '#9da4b4' : '#383b42'}
-        />
-      </TouchableHighlight>
-      <TouchableHighlight style={[style.cardButton, style.topRight]}>
+      <TouchableOpacity
+        style={[style.cardButton, style.topLeft]}
+        activeOpacity={(props.isShowTL || props.isShowFAIcon) ? 0 : 1}
+        disabled={(props.isShowTL || props.isShowFAIcon) ? false : true}
+        onPress={() => props.onPressTLC()}
+      >
+        {
+          props.isShowTL &&
+          <MCIcon
+            name={props.icon}
+            size={18}
+            style={style.buttonIconTopleft}
+            color={props.buttons.bottomRight.disabled ? '#9da4b4' : '#383b42'}
+          />
+        }
+        {
+          props.isShowFAIcon &&
+          <FAIcon
+            name={props.icon}
+            size={18}
+            style={style.buttonIconTopleft}
+            color={props.buttons.bottomRight.disabled ? '#9da4b4' : '#383b42'}
+          />
+        }
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => props.onPressTRC()}
+        style={[style.cardButton, style.topRight]}
+      >
         <FIcon
           name="info"
           size={18}
+          onPress={() => alert('Info Button pressed')}
           style={style.buttonIconTopRight}
           color={props.buttons.topRight.disabled ? '#9da4b4' : '#383b42'}
         />
-      </TouchableHighlight>
-      <TouchableHighlight style={[style.cardButton, style.bottomLeft]}>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => props.onPressBLC()}
+        style={[style.cardButton, style.bottomLeft]}
+      >
         <FIcon
           name="link-2"
           size={18}
           style={style.buttonIconBottomleft}
           color={props.buttons.bottomLeft.disabled ? '#9da4b4' : '#383b42'}
         />
-      </TouchableHighlight>
-      <TouchableHighlight style={[style.cardButton, style.bottomRight]} onPress={props.flip}>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => props.onPressBRC()}
+        style={[style.cardButton, style.bottomRight]}
+      >
         <MCIcon
           name="chart-line-variant"
-          size={18}
           size={18}
           style={style.buttonIconBottomRight}
           color={props.buttons.bottomRight.disabled ? '#9da4b4' : '#383b42'}
         />
-      </TouchableHighlight>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -63,11 +90,22 @@ CardButtons.defaultProps = {
       disabled: false,
       action: null
     },
-  }
+  },
+  onPressTLC: () => {},
+  onPressTRC: () => {},
+  onPressBRC: () => {},
+  onPressBLC: () => {},
+  isShowTL: false,
+  isShowFAIcon: false,
 };
 
 CardButtons.propTypes = {
-  flip: PropTypes.func,
+  onPressTLC: PropTypes.func,
+  onPressTRC: PropTypes.func,
+  onPressBRC: PropTypes.func,
+  onPressBLC: PropTypes.func,
+  isShowTL: PropTypes.bool,
+  isShowFAIcon: PropTypes.bool,
   buttons: PropTypes.shape({
     topLeft: PropTypes.shape({
       disabled: PropTypes.bool,
