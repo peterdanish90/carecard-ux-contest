5574c6d06fd0698bf4340e2d25102cf10aa9aa45
Vinay <vinaysinghsatna01@gmail.com>
2018-08-10 03:31:50 +0530

    Implement info and flip card action for ActiveMood card

--- src/cards/ActiveMood/index.js
+++ src/cards/ActiveMood/index.js
@@ -7,6 +7,8 @@ import style from './style';
 import CaptionImage from '../../components/CaptionImage';
 import { CARDS_NAME } from '../../constant';
 import { Colors } from '../../theme';
+import BackCard from '../../components/BackCard';
+import { CARDS_INFO } from '../../constant/cards';
 
 const flip = {
   0: {
@@ -35,7 +37,7 @@ class Reward extends Component {
     })
     setTimeout(() => {
       this.setState({isFlipping: false})
-    }, 1000)
+    }, 500)
   }
 
   handleShare = () => {
@@ -49,7 +51,7 @@ class Reward extends Component {
     return (
       <View style={style.container}>
         <Animatable.View animation={this.state.isFlipping ? flip : ''} style={style.cardContainer} duration={1000}>
-          {!this.state.isFlipping && (
+          {this.state.isFront && (
             <CardButtons
               isShowTL
               icon={"share-variant"}
@@ -60,36 +62,50 @@ class Reward extends Component {
               onPressBLC= {() => this.props.navigation.goBack()}
             />
           )}
-          <CaptionImage
-            animoji={{
-              name: `${this.state.myMood.name.toUpperCase()} ${this.state.animoji.name.toUpperCase()}`,
-              icon: this.state.myMood.icon,
-            }}
-            containerStyle={{ marginTop: 100 }}
-            iconStyle={style.icon}
-            textStyle={style.imageCaption}
-          />
-          <View style={style.messageContainer}>
-            <Text style={style.message}>{this.state.myMood.message}</Text>
-          </View>
-          <View style={style.buttonContainer}>
-            <Button
-              title="View Reward"
-              textStyle={{ fontSize: 16 }}
-              buttonStyle={{
-                height: 40,
-                backgroundColor: Colors.primaryColor,
-                width: 150,
-                alignItems: 'center',
-                justifyContent: 'center',
-                borderRadius: 20,
+          {
+            this.state.isFront ?
+            <CaptionImage
+              animoji={{
+                name: `${this.state.myMood.name.toUpperCase()} ${this.state.animoji.name.toUpperCase()}`,
+                icon: this.state.myMood.icon,
               }}
-              onPress={() => this.props.navigation.navigate(CARDS_NAME.reward, {
-                myMood: this.props.navigation.state.params.myMood,
-                animoji: this.props.navigation.state.params.animoji,
-              })}
+              containerStyle={{ marginTop: 100 }}
+              iconStyle={style.icon}
+              textStyle={style.imageCaption}
+            /> :
+            <BackCard
+              title={CARDS_INFO.active_mood.title}
+              desc={CARDS_INFO.active_mood.desc}
+              onClose={this.handleRotate}
             />
-          </View>
+          }
+          {
+            this.state.isFront &&
+            <View style={style.messageContainer}>
+              <Text style={style.message}>{this.state.myMood.message}</Text>
+            </View>
+          }
+          {
+            this.state.isFront &&
+            <View style={style.buttonContainer}>
+              <Button
+                title="View Reward"
+                textStyle={{ fontSize: 16 }}
+                buttonStyle={{
+                  height: 40,
+                  backgroundColor: Colors.primaryColor,
+                  width: 150,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 20,
+                }}
+                onPress={() => this.props.navigation.navigate(CARDS_NAME.reward, {
+                  myMood: this.props.navigation.state.params.myMood,
+                  animoji: this.props.navigation.state.params.animoji,
+                })}
+              />
+            </View>
+          }
         </Animatable.View>
       </View>
     )
