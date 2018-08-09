ca8b5453b61cb982120cebf6b27d3b5bb1bd830c
Vinay <vinaysinghsatna01@gmail.com>
2018-08-10 03:08:40 +0530

    Add cards title and desc constant for all

---
+++ src/constant/cards.js
@@ -0,0 +1,22 @@
+export const CARDS_INFO = {
+  choose_avatar: {
+    title: 'CHOOSE YOUR AVATAR',
+    desc: 'Keep track of your psychological state in a fun way! Every day select your avatar and your current mood, share with friends, family or your doctor and get rewarded.',
+  },
+  my_mood: {
+    title: 'MY MOOD TODAY',
+    desc: 'Scroll down and choose your current mood from the options available.',
+  },
+  active_mood: {
+    title: 'WHATS MY MOOD',
+    desc: 'See your recommendation for the mood selected. Tap View reward to see how much of the reward you get.',
+  },
+  reward: {
+    title: "TODAY'S REWARD",
+    desc: 'See your progress of accumulating reward points. View balance to see how many points can be exchanged to SOLVE tokens',
+  },
+  my_balance: {
+    title: 'MY BALANCE',
+    desc: 'See your available balance in reward points and SOLVE tokens. Tap the round button to exchange accumulated reward points to SOLVE tokens.',
+  },
+}
