4337e8b242efc83b1a133a326c36008c698fa126
Vinay <vinaysinghsatna01@gmail.com>
2018-08-10 03:10:40 +0530

    Implement info and flip card action for ChooseAvatar

--- src/cards/ChooseAvatar/index.js
+++ src/cards/ChooseAvatar/index.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import * as Animatable from 'react-native-animatable';
 import Swiper from 'react-native-swiper';
 import Header from '../../components/Header';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import CardButtons from '../../components/CardButtons';
 import style from './style';
 import { animojis } from '../../local';
 import CaptionImage from '../../components/CaptionImage';
 import { CARDS_NAME } from '../../constant';
 import { Colors } from '../../theme';
+import BackCard from '../../components/BackCard';
+import { CARDS_INFO } from '../../constant/cards';
 
 const flip = {
   0: {
@@ -36,14 +38,58 @@ class ChooseAvatar extends Component {
     })
     setTimeout(() => {
       this.setState({isFlipping: false})
-    }, 1000)
+    }, 500)
+  }
+
+  renderCard() {
+    return(
+      <View>
+        <Header
+          title={CARDS_INFO.choose_avatar.title}
+          style={{
+            marginTop: 40,
+            height: 100,
+            justifyContent: 'flex-start',
+            paddingTop: 20,
+          }}
+        />
+        <View
+          style={{
+            height: 250,
+            alignSelf: 'stretch',
+            backgroundColor: 'transparent',
+          }}
+        >
+          <Swiper
+            showsButtons={false}
+            loop
+            activeDotStyle={{
+              backgroundColor: Colors.primaryColor,
+            }}
+          >
+            {
+              this.state.animojis.map((animoji, index) => {
+                return (
+                  <CaptionImage
+                    key={animoji.face_acronym}
+                    animoji={animoji}
+                    onPress={animojiData => this.props.navigation.navigate(CARDS_NAME.my_mood, { animoji: animojiData })}
+                  />
+                );
+              })
+
+            }
+          </Swiper>
+        </View>
+      </View>
+    )
   }
 
   render() {
     return (
       <View style={style.container}>
         <Animatable.View animation={this.state.isFlipping ? flip : ''} style={style.cardContainer} duration={1000}>
-          {!this.state.isFlipping && (
+          {this.state.isFront&& (
             <CardButtons
               // flip={this.handleRotate}
               onPressTRC= {this.handleRotate} // info
@@ -51,43 +97,15 @@ class ChooseAvatar extends Component {
               onPressBLC= {() => this.props.navigation.navigate(CARDS_NAME.my_balance, { from: CARDS_NAME.choose_avatar })}
             />
           )}
-          <Header
-            title="CHOOSE YOUR AVATAR"
-            style={{
-              marginTop: 40,
-              height: 100,
-              justifyContent: 'flex-start',
-              paddingTop: 20,
-            }}
-          />
-          <View
-            style={{
-              height: 250,
-              alignSelf: 'stretch',
-              backgroundColor: 'transparent',
-            }}
-          >
-            <Swiper
-              showsButtons={false}
-              loop
-              activeDotStyle={{
-                backgroundColor: Colors.primaryColor,
-              }}
-            >
-              {
-                this.state.animojis.map((animoji, index) => {
-                  return (
-                    <CaptionImage
-                      key={animoji.face_acronym}
-                      animoji={animoji}
-                      onPress={animojiData => this.props.navigation.navigate(CARDS_NAME.my_mood, { animoji: animojiData })}
-                    />
-                  );
-                })
-
-              }
-            </Swiper>
-          </View>
+          {
+            this.state.isFront ?
+            this.renderCard():
+            <BackCard
+              title={CARDS_INFO.choose_avatar.title}
+              desc={CARDS_INFO.choose_avatar.desc}
+              onClose={this.handleRotate}
+            />
+          }
         </Animatable.View>
       </View>
     )
@@ -95,5 +113,3 @@ class ChooseAvatar extends Component {
 }
 
 export default ChooseAvatar;
-// https://exp.host/@vinaysinghsatna01/hayft
-// https://expo.io/artifacts/645c0c52-5558-4393-8058-c0f9b2540dbf
